d9d9697118713bc57c76d10f9062dfdc478b53de
Andy <andyeshiet@gmail.com>
2021-05-24 11:54:44 +0100

    added loginUIViews

---
+++ IosFirstApp/Resources/DatabaseManager.swift
@@ -0,0 +1,8 @@
+//
+//  DatabaseManager.swift
+//  IosFirstApp
+//
+//  Created by Android Developer on 24/05/2021.
+//
+
+import Foundation
---
+++ IosFirstApp/Resources/StorageManager.swift
@@ -0,0 +1,8 @@
+//
+//  StorageManager.swift
+//  IosFirstApp
+//
+//  Created by Android Developer on 24/05/2021.
+//
+
+import Foundation
